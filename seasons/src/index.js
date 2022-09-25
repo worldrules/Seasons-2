1ad64464b93a63115f1dc714f59e45b6b0b4e30b
worldrules <worldrulesdev@gmail.com>
2022-09-25 03:23:58 -0300

    class component init

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hi There!</div>;
-};
+class App extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+    
+    this.state = { lat: null }
+  }
+
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>{this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
