f834a6972e71a0842bfc6a5886bd9cfba246c42f
worldrules <worldrulesdev@gmail.com>
2022-09-27 02:40:16 -0300

    kek

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   // eslint-disable-next-line no-useless-constructor
@@ -19,7 +20,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading !</div>;
