34f46f38b047bf8827e41bcb7ab47c305304411f
worldrules <worldrulesdev@gmail.com>
2022-09-27 00:56:30 -0300

    componentDidMount added

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,29 +7,25 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-   if (this.state.errorMessage && this.state.lat) {
-    return <div>Error: {this.state.errorMessage}</div>
-   }
+    if (this.state.errorMessage && this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
 
-   if (!this.state.errorMessage && this.state.lat) {
-    return <div>Latitude: {this.state.lat}</div>
-   }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
 
-   return <div>Loading !</div>
-
-
+    return <div>Loading !</div>;
   }
 }
 
