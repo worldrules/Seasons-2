cb343044ada0dd0c3de0ede801f71ed62a7dcf2d
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2022-10-28 15:51:17 -0300

    kek

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,27 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else {
+    return lat < 0 ? "Winter" : "Summer";
+  }
+};
+
 const SeasonDisplay = (props) => {
-  console.log(props.lat)
-  return <div>SeasonDisplay</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "Winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  console.log(season);
+  const icon = season === "Winter" ? "snowflake" : "sun";
+
+  return (
+    <div className="">
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
