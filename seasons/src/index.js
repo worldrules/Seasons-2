959f6021944d1409179260f545972f14f4446363
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2022-11-03 11:40:51 -0300

    kek end

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   // eslint-disable-next-line no-useless-constructor
@@ -14,7 +15,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -23,8 +24,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading !</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
-
 ReactDOM.render(<App />, document.querySelector("#root"));
